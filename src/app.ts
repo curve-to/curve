5a83fa252e89f66ffbfaa863964f248e062585e5
Jiajun Yan <yanjiajun@ifanr.com>
2020-08-27 17:04:25 +0800

    feat: Remove files that are no longer needed

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,12 @@ import * as Koa from 'koa';
 import * as bodyParser from 'koa-body';
 import applyApiMiddleware from './routes';
 import { tokenValidation, jwtMiddleware } from './middleware/auth';
-import './config/database';
 import { bodyParserConfig } from './config';
 
 const app = new Koa();
 const port = 4000;
 
 app.use(bodyParser(bodyParserConfig)).use(tokenValidation).use(jwtMiddleware);
-
 applyApiMiddleware(app);
+
 app.listen(port);
