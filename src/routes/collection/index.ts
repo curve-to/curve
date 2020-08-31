e0b29edd6740b8a8cf8124980e9b9e9b752e9e57
realfrancisyan <domo36k@gmail.com>
2020-08-31 18:42:31 +0800

    refactor: Change method names for better documentation

--- src/routes/collection/index.ts
+++ src/routes/collection/index.ts
@@ -1,9 +1,9 @@
 import {
   create,
-  showDocuments,
-  showDocument,
+  getCollection,
+  getDocument,
   remove,
-  edit,
+  update,
 } from './controller';
 import { checkIdentity } from '../../middleware/validate';
 
@@ -11,19 +11,19 @@ const router = (Router) => {
   const router = new Router({ prefix: '/collection' });
 
   router
-    .get('/:collection/:documentId', showDocument) // get details of a document
+    .get('/:collection/:documentId', getDocument) // get details of a document
     .put(
       '/:collection/:documentId',
       checkIdentity({ requiresAdmin: true }),
-      edit
-    ) // edit a document
+      update
+    ) // update a document
     .delete(
       '/:collection/:documentId',
       checkIdentity({ requiresAdmin: true }),
       remove
     ) // remove a document
     .post('/:collection', checkIdentity({ requiresAdmin: true }), create) // create a document
-    .get('/:collection', showDocuments); // get documents of a collection
+    .get('/:collection', getCollection); // get documents of a collection
 
   return router;
 };
