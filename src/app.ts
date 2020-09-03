2799684ecbb7cc6cfc4667fab17337e690619687
realfrancisyan <domo36k@gmail.com>
2020-09-03 18:36:02 +0800

    feat: Add formatted json to response

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-body';
+import * as koaRes from 'koa-res';
 import applyApiMiddleware from './routes';
 import { tokenValidation, jwtMiddleware } from './middleware/auth';
 import { bodyParserConfig } from './config';
@@ -11,6 +12,7 @@ const port = 4000;
 app
   .use(bodyParser(bodyParserConfig))
   .use(corsMiddleware)
+  .use(koaRes())
   .use(tokenValidation)
   .use(jwtMiddleware);
 
